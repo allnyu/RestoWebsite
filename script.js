b6b18caf5e86aa897fd52abab14a212d7a5d9bee
Allen Ocomen <159407413+allnyu@users.noreply.github.com>
2024-05-10 19:33:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const navMenu = document.getElementById('nav_menu'), navToggle = document.getElementById('nav_toggle'), navClose = document.getElementById('nav_close')
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/* Menu show */
 if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classlist.add('show_menu')
-    })
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
 }
 
+/* Menu hidden */
 if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classlist.remove('show_menu')
-    })
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
 }
